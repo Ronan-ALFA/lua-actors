41514e610251d16cac4294b0db0b435cc6d9d492
Grant Beaty <gbeaty@gmail.com>
2015-02-26 18:52:23 -0500

    Fixed numbers of threads. Ask functionality.

--- actors.lua
+++ actors.lua
@@ -1,56 +1,72 @@
 local actors = { mt = {} }
 local lanes = require "lanes".configure()
+local actor_mt = { __index = mt }
+local MESSAGE_KEY = true
+local ADD_ACTOR_KEY = false
+local lindas = {}
+local acting
 
-local ACTOR_MESSAGE = true
-local STOP = -1
-local GET_STATE = -2
+local top_level_actor = { id = 0, linda = lanes.linda() }
+setmetatable(top_level_actor, { __index = actors.mt })
 
-local sender = nil
-
-function actors.mt.send(to, body)
-  to.linda:send(ACTOR_MESSAGE, { body = body, from = sender })
+function actors.mt.tell(to, msg)
+  to.linda:send(MESSAGE_KEY, { body = msg, to = to.id, from = acting or top_level_actor })
 end
 
-function actors.mt.stop(to)
-  to:send(STOP)
+function actors.mt.ask(to, msg, timeout)
+  local from = acting or top_level_actor
+  to:tell(msg)
+  local key, reply = top_level_actor.linda:receive(timeout, MESSAGE_KEY)
+  if reply then
+    return reply.body
+  end
+  return nil
 end
 
-function actors.mt.get_state(from)
-  to:send(GET_STATE)
-end
+function actors.system(num_threads, libs)
+  local system = {}
+  local num_actors = 0
+  local actor_lanes = {}
 
-local function process(self, linda, receive, state)
-  sender = sender or self
-  while true do
-    local key, msg = linda:receive(ACTOR_MESSAGE, 5)    
-    if msg == nil or msg == STOP then
-      break
-    elseif msg == GET_STATE then
-      local sender = msg.sender
-      if sender ~= nil then
-        sender:send(state)
-      else
-        -- Somehow return state to the 'main' lane?
+  local function process(linda)
+    local actor_states = {}
+    while true do
+      key, msg = linda:receive(50, ADD_ACTOR_KEY, MESSAGE_KEY)
+      if key == ADD_ACTOR_KEY then
+        actor_states[msg.id] = { state = msg.state, receive = msg.receive }
+      elseif key == MESSAGE_KEY then
+        local dest = actor_states[msg.to]        
+        if dest ~= nil then
+          acting = dest
+          dest.receive(dest.state, msg.body, msg.from)
+          acting = nil
+        end
       end
-    else
-      receive(state, msg)
     end
   end
-  return state
-end
 
-function actors.new(receive, state)
-  state = state or {}
-  local linda = lanes.linda()
-  local lane = lanes.gen("*", process)
+  function system.new(receive, state)
+    num_actors = num_actors + 1
+    local linda = lindas[math.random(#lindas)]
+    local actor = {
+      id = num_actors,
+      linda = linda
+    }
+    linda:send(ADD_ACTOR_KEY, { id = num_actors, state = state or {}, receive = receive })
+    setmetatable(actor, { __index = actors.mt })
+    return actor
+  end
 
-  local actor = {}
-  setmetatable(actor, { __index = actors.mt })
-  
-  actor.linda = linda
-  lane(actor, linda, receive, state)
+  libs = libs or "*"
 
-  return actor
+  for i = 1, num_threads do
+    actor_lanes[i] = lanes.gen(libs, process)
+    local linda = lanes.linda()
+    lindas[i] = linda
+    actor_lanes[i](linda)
+  end
+
+  return system
 end
 
 return actors
--- tests.lua
+++ tests.lua
@@ -1,19 +1,29 @@
 local actors = require "actors"
 
-local actor
-local outbox
-local function set_msg(state, msg)
-  state.msg = msg
-end
+local system
+describe("actor systems", function()
+  it("can be created", function()
+    -- This creates an actor system with two threads.
+    system = actors.system(2)
+  end)
+end)
 
+local actor1, actor2
+local outbox
 describe("actors", function()
   it("can be created", function()
-    actor = actors.new(set_msg)
+    -- This creates a new actor with the following receive function.
+    local function counter(state, body, from)
+      state.count = (state.count or 0) + 1
+      from:tell(state.count)
+    end
+    -- The actor is randomly assigned to one of the system's threads.
+    actor1 = system.new(counter)
+    actor2 = system.new(counter)
   end)
   it("receive messages", function()
     local msg = "Test!"
-    actor:send(msg)
-    actor:stop()
-    -- assert.are.same(actor:wait_death(), { msg = msg })
+    actor1:tell(msg)
+    assert.are.same(actor1:ask(msg,1), 1)
   end)
 end)
